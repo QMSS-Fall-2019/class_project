06aac8a22e7b01d70a6c5bac1e6d9acd818bb8d3
IshaShah27 <ishashah27@gmail.com>
2019-09-19 21:19:03 -0400

    Add files via upload

---
+++ hello_begin.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 21:14:56 2019
+
+@author: ishashah
+"""
+
+# hello my name is Isha Shah!
+# my favorite color is...orange?
